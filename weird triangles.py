546efbe039b94e49349f96cbbe034310b178054d
-CUTIE-WasTaken <91678642+moreCUTE@users.noreply.github.com>
2022-09-07 13:25:54 -0600

    Create weird triangles.py

---
+++ weird triangles.py
@@ -0,0 +1,56 @@
+
+c = 0
+while c <= 6:
+    for x in range(0,c+1):
+        print('* ', end = '')
+    c += 1
+    print('\n')
+
+c = 6
+while c >= 0:
+    for x in range(0,c+1):
+        print('* ', end = '')
+    c -= 1
+    print('\n')
+
+c = 6
+z = 0
+while z <= 6:
+    for x in range(0,c+1):
+        print('  ', end = '')
+    for x in range(0,z+1):
+        print('*   ', end = '')
+    z+=1
+    c -= 1
+    print('\n')
+    
+ c = 6
+ z = 0
+ while z <= 6:
+     for x in range(0,z+1):
+
+         print('  ', end = '')
+
+
+     for x in range(0,c+1):
+
+         print('*   ', end = '')
+     z+=1
+     c -= 1
+     print('\n')
+
+
+c = 6
+z = 0
+while z <= 6:
+    for x in range(0,c+1):
+
+        print('  ', end = '')
+
+
+    for x in range(0,z+1):
+
+        print('*   ', end = '')
+    z+=1
+    c -= 1
+    print('\n')
